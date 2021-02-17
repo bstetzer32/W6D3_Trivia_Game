e473881925159734308e0dbac52a4339371ea519
Nathan Blaz <nathanblaz@gmail.com>
2021-02-17 15:49:50 -0800

    Promise object and fetch phase complete

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+import { title } from 'process';
 import * as getClueFromPromise from 'promise-version.js';
 
 const usePromiseButton = document.getElementById('use-promise');
@@ -5,12 +6,26 @@ const questionDiv = document.getElementById('question');
 const answerDiv = document.getElementById('answer');
 const valueDiv = document.getElementById('value');
 const categoryTitleDiv = document.getElementById('category-title');
-
+const invalidCountDiv = document.getElementById('invalid-count');
 
 window.addEventListener("DOMContentLoaded", () => {
     usePromiseButton.addEventListener('click', () => {
         getClueFromPromise()
-            .then()
+            .then(res => {
+                questionDiv.innerHTML = res.question;
+                answerDiv.innerHTML = res.answer;
+                valueDiv.innerHTML = res.value;
+                categoryTitleDiv.innerHTML = res.category.title;
+                if (res.invalidCount && res.invalidCount > 0) {
+                    invalidCountDiv.innerHTML = 'invalid';
+                }
+                else {
+                    invalidCountDiv.innerHTML = 'valid';
+                }
+            })
+            .catch(err => {
+                console.error(err.message);
+            })
     })
 
 
